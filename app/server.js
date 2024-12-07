9673d4b98d0d2afac259b18e0dc050b6f1faef61
navid bahrami <navidbahrami1996@gmail.com>
2024-12-07 17:00:57 +0330

    server

--- app/server.js
+++ app/server.js
@@ -1,2 +1,10 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+dotenv.config()
+
+mongoose.connect(process.env.DB_URI).then(() => {
+    console.log('connected to DB');  
+}).catch(err => {
+    console.log(err?.message ?? 'DB connection is failed');
+    
+})
