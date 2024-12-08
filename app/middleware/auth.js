d3f810f64a1460b166213777668bd6bf120ceee6
navid bahrami <navidbahrami1996@gmail.com>
2024-12-08 11:39:04 +0330

    auth

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -5,5 +5,32 @@ var localStrategy = require('passport-local');
 var passport = require('passport');
 var {Doctor} = require('../../models/DoctorSchema');
 var {Patient} = require('../../models/PatientSchema');
+const {StatusCodes} = require('http-status-codes')
 const router = express.Router();
+
+
 //passport local strategy configuration
+passport.use('patient' , new localStrategy(async function verify(username , password , cb){
+    const querypatient = Patient.where({ username: username });
+
+    await querypatient.findOne().then(function (user){
+        if(user.password === password){
+            cb(null , user , {
+                status : StatusCodes.OK ,
+                message : 'login successful',
+            });
+        }else{
+            cb(null , false ,{
+                status : StatusCodes.UNAUTHORIZED,
+                message : 'Incorrect username or password',
+            });
+        }
+    })
+    .catch(function (err){
+        cb(null , false , {
+            status : StatusCodes.UNAUTHORIZED ,
+            message : 'Patient Does Not Exist',
+        });
+    });
+})
+);
