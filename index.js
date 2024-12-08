6d847d526b2213e1a705cc54e73fc910c628ccc4
navid bahrami <navidbahrami1996@gmail.com>
2024-12-08 13:54:11 +0330

    index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 var session = require('express-session');
 const MongoStore = require("connect-mongo");
 const port = 5000;
@@ -8,6 +9,12 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+mongoose.connect('mongodb://localhost:27017/hospitalAppointment').then(()=>{
+    console.log('successfully connected to mongodb');
+}).catch((err) =>{
+    console.log('failed to connect to database :' + err);
+})
+const db = mongoose.connection
 //session configuration
 app.use(session({
     secret: 'gfgsecret',
@@ -23,8 +30,8 @@ app.use(session({
     })
 }));
 
-app.use('/', authRouter);
-app.use('/', hospitalRouter);
+//app.use('/', authRouter);
+//app.use('/', hospitalRouter);
 
 //server listening
 app.listen(port, () => {
