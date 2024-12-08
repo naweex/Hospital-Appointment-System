58553fa64b0483423d2a5b92bd280d5f303fa5d1
navid bahrami <navidbahrami1996@gmail.com>
2024-12-08 11:50:03 +0330

    auth

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -34,3 +34,49 @@ passport.use('patient' , new localStrategy(async function verify(username , pass
     });
 })
 );
+
+passport.use(
+    "doctor",
+    new LocalStrategy(async function verify(username, password, cb) {
+        const querydoctor = Doctor.where({ username: username });
+        await querydoctor
+            .findOne()
+            .then(function (user) {
+                if (user.password === password) {
+                    cb(null, user, {
+                        status: 200,
+                        message: "Login Successful.",
+                    });
+                } else {
+                    cb(null, false, {
+                        status: 401,
+                        message: "Incorrect username or password.",
+                    });
+                }
+            })
+            .catch(function (err) {
+                cb(null, false, {
+                    status: 401,
+                    message: "Doctor Does Not Exist",
+                });
+            });
+    })
+);
+
+//passport jwt strategy configuration
+passport.use(
+    new JWTStrategy(
+    {
+        jwtFromRequest: (req) => req.headers["x-access-token"],
+            secretOrKey: "secretKeeeey",
+    },
+    (payload, done) => {
+        return done(null, payload);
+    }
+    )
+);
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, { id: user.id, username: user.username });
+    });
+});
